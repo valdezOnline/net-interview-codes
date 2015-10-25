d855753ba9ed5d389cf58c7c4e063c0a9339065f
valdezOnline <joel.valdez@valdezonline.com>
2015-10-24 22:19:11 -0700

    Initial Files

---
+++ 2inputset1output/InputReceived.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2inputset1output
+{
+    public class InputReceived
+    {
+        public double dblValue { get; set; }
+        public string strValue { get; set; }
+
+        public bool ValidateNbrInput(string nbrInput)
+        {
+            Regex regexNbr = new Regex(@"(\+|\-)?(\d*\.)?\d+$");
+            return regexNbr.IsMatch(nbrInput);
+        }
+        public bool ValidateLtrInput(string ltrString)
+        {
+            Regex regexLtr = new Regex(@"[a-j]|[A-J]");
+            return regexLtr.IsMatch(ltrString);
+        }
+
+        public double ReturnLetterValue(string letterString)
+        {
+
+            switch (letterString.ToUpper().ToString())
+            {
+                case "A":
+                    return 0;                    
+                case "B":
+                    return 1;
+                case "C":
+                    return ReturnLetterValue("B");
+                case "D":
+                    return ReturnLetterValue("B") + ReturnLetterValue("C");
+                case "E":
+                    return ReturnLetterValue("C") + ReturnLetterValue("D");
+                case "F":
+                    return ReturnLetterValue("D") + ReturnLetterValue("E");
+                case "G":
+                    return ReturnLetterValue("E") + ReturnLetterValue("F");
+                case "H":
+                    return ReturnLetterValue("F") + ReturnLetterValue("G");
+                case "I":
+                    return ReturnLetterValue("G") + ReturnLetterValue("H");
+                case "J":
+                    return ReturnLetterValue("G") + ReturnLetterValue("H") + ReturnLetterValue("F")+ ReturnLetterValue("G");
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ 2inputset1output/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2inputset1output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            InputsCollection entries = new InputsCollection();
+            
+            // Enter first-set (Numbers Only):
+            Console.WriteLine("Enter first-set (Numbers Only [positive/negative or decimals])...Enter 5 times");
+                        
+            for (int i = 0; i < 5; i++)
+            {
+                InputReceived entry = new InputReceived();
+                string input = Console.ReadLine();
+                if (entry.ValidateNbrInput(input))
+                {
+                    entry.dblValue = Convert.ToDouble(input);
+                    entry.strValue = input;
+                    entries.Add(entry);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Number Entered!");
+                }
+            }
+
+            // Enter first-set (Letters Only):
+            Console.WriteLine("Enter second-set (Letters Only [a-j])...Enter 5 times");
+
+            for (int i = 0; i < 5; i++)
+            {
+                InputReceived entry = new InputReceived();
+                string input = Console.ReadLine();
+                if (entry.ValidateLtrInput(input))
+                {
+                    entry.dblValue = Convert.ToDouble(entry.ReturnLetterValue(input));
+                    entry.strValue = input;
+                    entries.Add(entry);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Letter Entered!");
+                }
+            }
+
+            // Sort Entries
+            var sortedEntries = from entry in entries
+                                orderby entry.dblValue, entry.strValue
+                                select entry.strValue;
+
+            // Display Sorted Entries
+            Console.WriteLine();
+            Console.WriteLine("Sorted Output...");
+
+            foreach (string msg in sortedEntries)
+            {                
+                Console.WriteLine(' ' + msg);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
